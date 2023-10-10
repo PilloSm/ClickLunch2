0fad0c9ffc76b1fb2e50eb668a70c3911b00d0da
PillSm <corzo.arguello.luis@gmail.com>
2023-10-10 17:57:59 -0500

    Hola

--- validacion.js
+++ validacion.js
@@ -1,17 +1,36 @@
-const val= {};
+const vals= {};
 
-console.log("Este es mi validacion")
-
+console.log("Este es mi validacion");
+var pattern ="";
 function valNum (x){
-    if (x.isNaN){
+    if (isNaN(x)){
         console.log("Error");
+        return false;
     }
     else {
         return true;
     }
+}
+
+function valPass(x){
+    if(x.length>8 && x.length<30){
+        return true;
+    }
+    else{
+        console.log("Error de longitud");
+        return false;
+    }
+}
+
+function valCorreo(x){
+    console.log("En proceso de");
+}
+
+function valString(x){
+    console.log("En proceso");
 
 }
-val.valNum=valNum();
 
-
+vals.valNum=valNum();
+vals.valPass=valPass();
 module.exports=val();
